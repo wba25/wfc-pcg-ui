1ea4a027fa90f49e39d6f49fc89aac740587a8c2
wellington <wba@cin.ufpe.br>
2023-04-23 14:08:06 -0300

    Use v-show to optimize nav loading

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,6 +1,6 @@
 <template>
-  <NewTiles v-if="getRegisterStage == 0"/>
-  <NewNeighbors v-else/>
+  <NewTiles v-show="getRegisterStage == 0"/>
+  <NewNeighbors v-show="getRegisterStage == 1"/>
 </template>
 
 <script>
